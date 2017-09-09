2010bdca67f595a059d8d6918191ee2a71095a4a
kiiiiNNNNNNNNNyyyy <ad.sam007@gmail.com>
2017-09-09 14:51:05 -0400

    Implemented components

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Components{
+    render(){
+        return(
+            <div>
+                My App
+            </div>
+        )
+    }
+}
+
+export default App;

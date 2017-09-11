e38a8c1b85494cb95f4c447388761dd2632820e7
Arjun  Dass <ad.sam007@gmail.com>
2017-09-10 22:56:33 -0500

    Added first jsx file

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends Components{
+class App extends Component{
     render(){
         return(
             <div>

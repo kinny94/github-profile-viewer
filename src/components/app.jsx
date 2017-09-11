afcf08dbf74192977201d74d8bd9349b328a216f
Arjun  Dass <ad.sam007@gmail.com>
2017-09-10 23:59:10 -0500

    Added essential components and github app credentials

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,17 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            username: 'kinny94',
+            userData: [],
+            userRepos: [],
+            perPage: 5 
+        } 
+    }
+     
     render(){
         return(
             <div>
@@ -9,6 +20,16 @@ class App extends Component{
             </div>
         )
     }
+}
+
+App.propTypes = {
+    clientId: React.PropTypes.string,
+    clientSecret: React.PropTypes.string
+};
+
+App.defaultProps = {
+    clientId: 'Iv1.44ef32c73adc3386',
+    clientSecret: 'e7d88b105d5f6a5ca002b61ff53008ebeab9bf7d'
 }
 
 export default App;

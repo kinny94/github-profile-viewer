389ef2a349d0c1f0a4adbd794a6a27df71521c99
kiiiiNNNNNNNNNyyyy <ad.sam007@gmail.com>
2017-09-11 16:22:29 -0400

    Added search bar text box

---
+++ src/components/github/search.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class Search extends Component{
+    
+    onSubmit(e){
+        e.preventDefault();
+        console.log('Submitted!!');
+    }
+
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <label>Search Github Users </label>
+                    <input type="text" ref="username" className="form-control" />
+                </form> 
+            </div>
+        )
+    }
+}
+
+export default Search;
